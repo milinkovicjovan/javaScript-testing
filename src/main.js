b65ff1b134e12b50d38d17b4f34930113fc7c85c
Jovan Milinkovic <milinkovicjovanbusiness@gmail.com>
2022-07-21 15:51:03 +0200

    jest_testing

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@
 //   return await fetch('http://google.com');
 // };
 
+const { default: axios } = require('axios');
+
 ////////////////////////////////////////////
 ////////////////////////////////////////////
 // function sum(a, b) {
@@ -25,9 +27,38 @@
 //////////////////////////////////////////
 //////////////////////////////////////////
 
-function subtract(a, b) {
-  return a - b;
-}
+// function subtract(a, b) {
+//   return a - b;
+// }
 
-// eslint-disable-next-line no-undef
-module.exports = subtract;
+// // eslint-disable-next-line no-undef
+// module.exports = subtract;
+
+//////////////////////////////////////////
+//////////////////////////////////////////
+
+// CHECK FOR TRUTHY & FALSY VALUES
+// toBeNull matches only null
+// toBeUndefined matches only undefined
+// toBeDefined is the opposite of toBeUndefines
+// toBeTruthy matches anything that an if statement treats as true
+// toBeFalsy matches anything that an if statement treats as false
+
+// const axios = require('axios');
+
+const functions = {
+  isNull: () => null,
+  checkValue: (x) => x,
+  createUser: () => {
+    const user = { firstName: 'Brad' };
+    user['lastName'] = 'Traversy';
+    return user;
+  },
+  fetchUser: () =>
+    axios
+      .get('https://jsonplaceholder.typicode.com/users/1')
+      .then((res) => res.data)
+      .catch((err) => 'error'),
+};
+
+module.exports = functions;
--- src/main.test.js
+++ src/main.test.js
@@ -18,8 +18,66 @@
 
 //////////////////////////////////////////
 //////////////////////////////////////////
-const subtract = require('./main');
+// const subtract = require('./main');
 
-test('properly subtracting two numbers', () => {
-  expect(subtract(1, 2)).toBe(-1);
+// test('properly subtracting two numbers', () => {
+//   expect(subtract(1, 2)).toBe(-1);
+// });
+
+//////////////////////////////////////////
+//////////////////////////////////////////
+
+const functions = require('./main');
+
+// toBeNull
+test('Should be null', () => {
+  expect(functions.isNull()).toBeNull();
+});
+
+// toBeFalsy
+test('Should be falsy', () => {
+  expect(functions.checkValue(undefined)).toBeFalsy();
+});
+
+// toEqual
+test('User should be Brad Traversy object', () => {
+  expect(functions.createUser()).toEqual({
+    firstName: 'Brad',
+    lastName: 'Traversy',
+  });
+});
+
+//Less than and greater than
+test('Should be under 1600', () => {
+  const load1 = 800;
+  const load2 = 700;
+  expect(load1 + load2).toBeLessThanOrEqual(1600);
+});
+
+// Regex (regular expressions)
+test('There is no I in team', () => {
+  expect('team').not.toMatch(/I/i);
+});
+
+// Arrays
+// test('Admin should be in usernames', () => {
+//   usernames = ['john', 'karen', 'admin'];
+//   expect(usernames).toContain('admin');
+// });
+
+// Working with async data
+
+// Promise
+// test('User fetched name should be Leanne Graham', () => {
+//   expect.assertions(1);
+//   return functions.fetchUser().then((data) => {
+//     expect(data.name).toEqual('Leanne Graham');
+//   });
+// });
+
+// Async await
+test('User fetched name should be Leanne Graham', async () => {
+  expect.assertions(1);
+  const data = await functions.fetchUser();
+  expect(data.name).toEqual('Leanne Graham');
 });
